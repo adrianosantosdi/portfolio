80448a5571c7e27fa95c967f412ba1e25523f080
Adriano Santos <adrianosantosdi@gmail.com>
2020-12-19 15:30:48 -0300

    Concluido a section banner do site

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-const lista = document.querySelector('ul');
-const novo_lista = document.createElement('li');
-lista.appendChild(novo_lista)
+let quantidade = $('.testemunhos ul').length;
+let limite = (quantidade * 336 ) * -1;
+let largura = limite + 336;
+let view_largura = $('.testemunha-centro').width();
+
+$('.testemunhos').css('width', limite*-1);
+
+$('.teste').html(limite)
+
+$(".testemunha-direita").click(function(){
+    posicao_atual = $('.testemunhos').position().left;
+    nova_posicao = posicao_atual - view_largura;
+    if (limite < posicao_atual - view_largura) {
+        $('.testemunhos').animate({left:nova_posicao}, 1000);
+        
+    }
+
+})
+$(".testemunha-esquerda").click(function(){
+    posicao_atual = $('.testemunhos').position().left;
+    if (posicao_atual + view_largura <= 0) {
+        nova_posicao = posicao_atual + view_largura;
+        $('.testemunhos').animate({left:-1010}, 1000);
+    }
+
+})
