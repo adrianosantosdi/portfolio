d82b8b313d61386158cf521748dcd89809799133
Adriano Santos <adrianosantosdi@gmail.com>
2020-12-21 12:37:17 -0300

    Concluido a section footer do portfolio

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
-let quantidade = $('.testemunhos ul').length;
-let limite = (quantidade * 336 ) * -1;
-let largura = limite + 336;
-let view_largura = $('.testemunha-centro').width();
+$(function () {
+    let quantidade = $('.testemunhos ul').length;
+    let limite = (quantidade * 336) * -1;
+    let largura = limite + 336;
+    let view_largura = $('.testemunha-centro').width();
 
-$('.testemunhos').css('width', limite*-1);
+    $('.testemunhos').css('width', limite * -1);
 
-$('.teste').html(limite)
+    $('.teste').html(limite)
 
-$(".testemunha-direita").click(function(){
-    posicao_atual = $('.testemunhos').position().left;
-    nova_posicao = posicao_atual - view_largura;
-    if (limite < posicao_atual - view_largura) {
-        $('.testemunhos').animate({left:nova_posicao}, 1000);
-        
-    }
+    $(".testemunha-direita").click(function () {
+        posicao_atual = $('.testemunhos').position().left;
+        nova_posicao = posicao_atual - view_largura;
+        if (limite < posicao_atual - view_largura) {
+            $('.testemunhos').animate({ left: nova_posicao }, 1000);
 
+        }
+
+    })
+    $(".testemunha-esquerda").click(function () {
+        posicao_atual = $('.testemunhos').position().left;
+        if (posicao_atual + view_largura <= 0) {
+            nova_posicao = posicao_atual + view_largura;
+            $('.testemunhos').animate({ left: -1010 }, 1000);
+        }
+
+    })
+
+  
 })
-$(".testemunha-esquerda").click(function(){
-    posicao_atual = $('.testemunhos').position().left;
-    if (posicao_atual + view_largura <= 0) {
-        nova_posicao = posicao_atual + view_largura;
-        $('.testemunhos').animate({left:-1010}, 1000);
-    }
 
-})
