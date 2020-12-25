4807f9d9a220083dec5c7cc15138f1dd2fe598e6
Adriano Santos <adrianosantosdi@gmail.com>
2020-12-25 14:30:42 -0300

    concluido

--- script.js
+++ script.js
@@ -26,6 +26,94 @@ $(function () {
 
     })
 
-  
+    $('.testemunhos ul:eq(0)').css('opacity', 0)
+    $('.testemunhos ul:eq(1)').css('opacity', 0)
+    $('.testemunhos ul:eq(2)').css('opacity', 0)
+
+    $('.domingo-perfeito').waypoint(function(direcao){
+        if (direcao == "down") {
+            $('.folha1').addClass('animate__animated animate__fadeInUp')
+            $('.folha3').addClass('animate__animated animate__fadeInUp')
+            $('.camarao').addClass('animate__animated animate__fadeInUp animate__slow')
+            $('.tomate').addClass('animate__animated animate__fadeInUp animate__slow')
+            $('.folha2').addClass('animate__animated animate__fadeInUp animate__slow')
+            $('.queijo').addClass('animate__animated animate__fadeInUp animate__slow')
+        }
+        if (direcao == "up") {
+            $('.folha1').removeClass('animate__animated animate__fadeInUp')
+            $('.folha3').removeClass('animate__animated animate__fadeInUp')
+            $('.camarao').removeClass('animate__animated animate__fadeInUp animate__slow')
+            $('.tomate').removeClass('animate__animated animate__fadeInUp animate__slow')
+            $('.folha2').removeClass('animate__animated animate__fadeInUp animate__slow')
+            $('.queijo').removeClass('animate__animated animate__fadeInUp animate__slow') 
+        }
+    })
+
+}, {
+    offset: '450px;'
 })
+
+$('.pizza').waypoint(function(direcao) {
+    if (direcao == 'down') {
+            $('.pizza p').addClass('animate__animated animate__fadeInUp')
+            $('.pizza .pizza-opcoes').addClass('animate__animated animate__fadeInUp animate__slow')
+    }
+
+    if (direcao == "up") {
+        $('.pizza p').removeClass('animate__animated animate__fadeInUp')
+        $('.pizza .pizza-opcoes').removeClass('animate__animated animate__fadeInUp animate__slow')
+    }
+}, {
+    offset: '450px;'
+})
+
+$('.testemunha').waypoint(function(direcao){
+    if (direcao == "down") {
+        $('.testemunhos ul:eq(0)').addClass('animate__animated animate__fadeInUp animate__slow')
+        $('.testemunhos ul:eq(1)').addClass('animate__animated animate__fadeInUp animate__slow')
+        $('.testemunhos ul:eq(2)').addClass('animate__animated animate__fadeInUp animate__slow')
+    }
+
+    if (direcao == 'up') {
+        $('.testemunhos ul:eq(0)').removeClass('animate__animated animate__fadeInUp animate__slow')
+        $('.testemunhos ul:eq(1)').removeClass('animate__animated animate__fadeInUp animate__slow')
+        $('.testemunhos ul:eq(2)').removeClass('animate__animated animate__fadeInUp animate__slow')
+    }
+}, {
+    offset: '400px;' 
+})
+
+$('.transicao-testemunha-plana').waypoint(function(direcao){
+    if (direcao == 'down') {
+        $('.decoracao1').addClass('animate__animated animate__fadeInLeft animate__slow')
+        $('.decoracao2').addClass('animate__animated animate__fadeIn animate__slow')
+        $('.decoracao3').addClass('animate__animated animate__fadeInRight animate__slow')
+    }else {
+
+        $('.decoracao1').removeClass('animate__animated animate__fadeInLeft animate__slow')
+        $('.decoracao2').removeClass('animate__animated animate__fadeIn animate__slow')
+        $('.decoracao3').removeClass('animate__animated animate__fadeInRight animate__slow')
+    }    
+
+},{
+    offset: '800px;'
+}) 
+
+
+$('.plano-principal').waypoint(function(direcao){
+    if (direcao == "down") {
+      $('.planos:eq(0)').addClass('animate__animated animate__fadeInLeft animate__slow')  
+      $('.planos:eq(1)').addClass('animate__animated animate__fadeInUp animate__slow')  
+      $('.planos:eq(2)').addClass('animate__animated animate__fadeInRight animate__slow')  
+    }else {
+         $('.planos:eq(0)').removeClass('animate__animated animate__fadeInLeft animate__slow')  
+         $('.planos:eq(1)').removeClass('animate__animated animate__fadeInUp animate__slow')  
+        $('.planos:eq(2)').removeClass('animate__animated animate__fadeInRight animate__slow')
+    }
+}, {
+    offset: '500px;'
+})
+
+
+
 
